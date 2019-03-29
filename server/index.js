ab1109db7219f99a233e534316675e57734f21a4
javo <interactionmx@gmail.com>
2019-03-28 19:44:58 -0600

    added static folder and prepared SPA

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts);
 
+// handle production 
+if (process.env.NODE_ENV === 'production') {
+    // static folder for production
+    app.use(express.static(__dirname + '/public'));
+
+    // handle SPA routes behavior
+    app.get(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'));
+
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port $ ${port}`));
