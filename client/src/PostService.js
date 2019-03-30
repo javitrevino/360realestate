cc504d728b7757d03a12ce19d7afcf446f3ff7ec
javo <interactionmx@gmail.com>
2019-03-29 23:04:49 -0600

    removed submodule to git

---
+++ client/src/PostService.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const url = "api/posts/";
+
+class PostService {
+    // get posts 
+
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch (err) {
+                reject(err);
+
+            }
+        });
+    }
+
+
+    //create post
+
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        })
+    }
+
+    //delete posts
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`);
+    }
+
+
+}
+
+export default PostService;
---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <h1>Publicar y borrar posts de MongoDB Atlas DB</h1>
+    <h2>Apex</h2>
+    <div class="create-post">
+      <label for="create-post">Nombre...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Crear un post">
+      <button v-on:click="createPost">Publicar Escena 3D</button>
+    </div>
+    <hr>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="post-container">
+      <div
+        class="post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+        v-on:dblclick="deletePost(post._id)"
+      >
+        {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
+        <p class="text">{{post.text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostService from "../PostService";
+
+export default {
+  name: "PostComponent",
+  data() {
+    return {
+      posts: [],
+      error: "",
+      text: ""
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+
+    async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
+    }
+  }
+};
+</script>
+
+<!-- EJEMPLO DE COMO FUNCIONA Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div.container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+p.error {
+  border: 1px solid red;
+  background-color: pink;
+  padding: 10px;
+  margin-bottom: 15px;
+}
+
+div.post {
+  position: relative;
+  border: 1px solid green;
+  background: lightblue;
+  padding: 10px 10px 30px 10px;
+}
+
+div.created-at {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 5px 15px 5px 15px;
+  background-color: darkgreen;
+}
+
+p.text {
+  font-size: 12px;
+  font-weight: 700;
+  margin-bottom: 0;
+}
+</style>
